7338e23483678afc3455ea0c6a3e0ed7967a6b2a
Jean Chassoul <chassoul@gmail.com>
2017-07-13 17:01:54 -0400

    init install command

--- cube-cli.lua
+++ cube-cli.lua
@@ -11,5 +11,36 @@ local uuid = require("uuid")
 -- init uuid random seed
 uuid.randomseed(socket.gettime()*10000)
 
--- System UUID
-local system_uuid = uuid()
+-- Session UUID
+local session_uuid = uuid()
+
+local parser = argparse() {
+   name = "cube-cli",
+   description = "An artifact discovered full of https://cloudforest.ws seeds.",
+   epilog = "Research of the Cube's function are still ongoing. \nFor more info, see https://nonsense.ws."
+}
+
+parser:option("-b --build", "build node on location", "/opt/treehouse")
+
+parser:command_target("command")
+parser:command("install")
+parser:command("status")
+parser:command("remove")
+parser:command("update")
+parser:command("upgrade")
+
+local args = parser:parse()
+
+print(args)
+
+if args['command'] == 'install' then
+    print('Installing the shit out if this node')
+    --os.execute("mkdir " .. args['build'])
+    
+    os.execute("git clone https://github.com/nonsensews/treehouse " .. args['build'])
+    os.execute("curl -O https://erlang.mk/erlang.mk")
+    os.execute("mv erlang.mk " .. args['build'])
+    os.execute("cd " .. args['build'] .." && make all")
+else
+	print('do something else')
+end
